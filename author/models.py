27634580b47292dd38f68fd7ff1a92222b3317c1
onterio <karpovfil@yandex.ru>
2020-12-18 02:07:03 +0500

    Initial commit

---
+++ author/models.py
@@ -0,0 +1,15 @@
+# Create your models here.
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField("Имя",max_length=100)
+    age = models.positiveSmallIntegerField("Возраст",default=0)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="authors/")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Авторы"
+        verbose_name_plural = "Авторы"
